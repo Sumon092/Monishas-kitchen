ce5e2efdb19876c4ec2d82a34c1590ebdd534a1f
sumon092 <m.sumon@outlook.com>
2022-04-18 23:02:23 +0600

    about me added

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div>
-            <h2>This is about</h2>
+        <div className='container'>
+            <h2>Hi there</h2>
+            <h3>I am Mohammad Sumon</h3>
+            <p className='fs-3'>I am a Electrical Engineer who want to change his career path desparatly to as web development.I am working hard to achive my goal.now I am a learner of programming hero web developement coures.It showing my path I have wanted and guiding me proper way.I think I will be successful if I continue my learning process that I have now.</p>
         </div>
     );
 };
